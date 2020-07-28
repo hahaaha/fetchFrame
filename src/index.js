9f1507c2212338dfd832ebdb739184a79eab142a
haha <390519764@qq.com>
2020-07-28 19:21:46 +0800

    feature: add timeout function

--- src/index.js
+++ src/index.js
@@ -1,38 +1,46 @@
 let fetchFrame = function () {
-
+    this.controller = new AbortController()
+    this.signal = this.controller.signal
+    this.timeoutPromise = (timeout) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(new Response("timeout", { status: 504, statusText: "timeout " }))
+                this.controller.abort();
+            }, timeout)
+        })
+    }
 }
 
+
+
 fetchFrame.prototype.req = function (data) {
-    let params = {}
+    let params = { signal: this.signal }
     let url = ""
     let headers = new Headers()
     headers.append('Content-Type', 'application/json')
 
-    if(data.url) {
+    if (data.url) {
         url = data.url
     }
 
-    if(data.data) {
+    if (data.data) {
         params.body = JSON.stringify(data.data)
     }
 
-    if(data.method) {
-        params.method = data.method
+    if (data.method) {
+        params.method = data.method.toUpperCase()
     }
 
-    // for (let key in data) {
-    //     if (key === "url") {
-    //         url = data[key]
-    //     } else if (key === "data") {
-    //         params.body = JSON.stringify(data[key])
-    //     } else {
-    //         params[key] = data[key]
-    //     }
-    // }
     params.headers = headers
     console.log(params)
-    return fetch(url, params).then((response) => {
-        return response.json()
-    })
+    return Promise.race([this.timeoutPromise(data.timeout), fetch(url, params)])
+        .then((Response) => {
+            console.log(Response)
+            if(Response.status === 504) {
+                throw "timeout"
+            } else {
+                return Response.json()
+            }
+        })
 }
 export default fetchFrame
--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 import fetchFrame from './index.js'
 
 let b = new fetchFrame()
+// b.req({
+//     url: "http://localhost:5000/role/5ee3107a451977654df67399",
+//     method: "patch",
+//     data: {
+//         name: "tupian",
+//     }
+// }).then((res) => {
+//     console.log(res)
+// })
+
+// b.req({
+//     url: "http://localhost:5000/role/5ee3107a451977654df67399",
+//     method: "delete"
+// }).then((res) => {
+//     console.log(res)
+// })
+
 b.req({
-    url: "http://localhost:5000/tag",
+    url: "http://localhost:5000/users/login",
     method: "post",
+    timeout: 1000,
     data: {
-        name: "标签d"
+        userName: "admin1",
+        password: "admin"
     }
 }).then((res) => {
     console.log(res)
